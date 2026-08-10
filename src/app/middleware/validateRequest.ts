277d8f7211d7190fa867ba65b018b5066b5b863d
Hayder Ali <hayderbd4290@gmail.com>
2026-08-10 20:30:41 +0600

    create validtion schema with zod validation

---
+++ src/app/middleware/validateRequest.ts
@@ -0,0 +1,23 @@
+import type z from "zod";
+import { catchAsync } from "../utils/catchAsync";
+import type { NextFunction, Request, Response } from "express";
+
+export const validateRequest = (zodSchema: z.ZodObject) => {
+	return catchAsync((req: Request, res: Response, next: NextFunction) => {
+		const payload = req.body;
+
+		const result = zodSchema.safeParse(payload);
+
+		const errorZodMessage = result?.error?.issues
+			.map((issue) => issue.message)
+			.join(", ");
+
+		if (!result.success) {
+			throw new Error(errorZodMessage);
+		}
+
+		req.body = result.data;
+
+		next();
+	});
+};
---
+++ src/app/module/auth/auth.validation.ts
@@ -0,0 +1,39 @@
+import z from "zod";
+
+const PatientRegistrationZodSchema = z.object({
+	name: z
+		.string("Not String!!!")
+		.min(3, "Minimum 3 Character")
+		.max(50, "maximum 50 Character long"),
+	email: z.email("Give Valid Email"),
+	password: z
+		.string()
+		.min(8, "Password Must Minimum 8 Characters Long.")
+		.regex(/[a-z]/, "Password must contain atleast 1 Lowercase Letter")
+		.regex(/[A-Z]/, "Password must contain atleast 1 Uppercase Letter")
+
+		.regex(/[0-9]/, "Password must contain atleast 1 Number")
+		.regex(/[^A-Za-z0-9]/, "Password must contain atleast 1 Special Character"),
+	patient: z
+		.object({
+			contactNumber: z.string().optional(),
+		})
+		.optional(),
+});
+
+const LoginZodSchema = z.object({
+	email: z.email(),
+	password: z
+		.string()
+		.min(8, "Password Must Minimum 8 Characters Long.")
+		.regex(/[a-z]/, "Password must contain atleast 1 Lowercase Letter")
+		.regex(/[A-Z]/, "Password must contain atleast 1 Uppercase Letter")
+
+		.regex(/[0-9]/, "Password must contain atleast 1 Number")
+		.regex(/[^A-Za-z0-9]/, "Password must contain atleast 1 Special Character"),
+});
+
+export const UserValidation = {
+	PatientRegistrationZodSchema,
+	LoginZodSchema,
+};
