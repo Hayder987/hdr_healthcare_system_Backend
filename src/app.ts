fff5a14f64afe1e7d5d65a11583acef1a37271cf
Hayder Ali <hayderbd4290@gmail.com>
2026-08-18 02:07:30 +0600

    create test book appoinment and callback with bkash

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,8 @@ import { redisClient } from "./app/lib/redis";
 import crypto from "crypto";
 import { authRoutes } from "./app/module/auth/auth.route";
 import { UserRoutes } from "./app/module/user/user.routes";
+import { getBkashIdToken } from "./app/lib/bkash";
+import { AppointementRoutes } from "./app/module/appointment/appointment.route";
 
 
 const app: Application = express();
@@ -34,6 +36,7 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", UserRoutes);
+app.use("/api/v1/appointment", AppointementRoutes);
 
 
 // test api for development
@@ -41,20 +44,13 @@ app.get("/test", async (req: Request, res: Response, next : NextFunction) => {
 
 	try {
 
-		const otp = crypto.randomInt(100000, 1000000)
-	    
-		// await redisClient.set("forgot-password-otp:patient1@gmail.com", "123456", {
-		// 		expiration : {
-		// 			type : "EX",
-		// 			value : 5 * 60
-		// 		}
-		// 	})
+		const grantIdTokenResult = await getBkashIdToken();
 
 
 		res.status(httpStatus.OK).json({
 			success: true,
 			message: "Welcome to PH Healthcare System Backend",
-			data : {otp}
+			data : grantIdTokenResult,
 		});
 	} catch (error) {
 		console.log(error);
